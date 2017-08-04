b9e0b46e9fb41f2ab5a8406eb03f411792e4c667
stevenliuyi <stevenliuyi@gmail.com>
2017-08-04 16:50:11 -0400

    add npm packages related to React

--- install/npm.sh
+++ install/npm.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # install npm packages
-packages="create-react-app"
+packages="create-react-app prop-types escape-string-regexp sort-by react-router-dom form-serialize"
 for package in $packages; do
     npm install -g "$packages"
 done
