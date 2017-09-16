038009d50da1835c769943a6991518c9100ce978
stevenliuyi <stevenliuyi@gmail.com>
2017-09-16 11:16:00 -0400

    add MacVim

--- install/macos_brew.sh
+++ install/macos_brew.sh
@@ -21,5 +21,5 @@ apps=(autojump awscli cmake ffmpeg git p7zip tmux tmux-mem-cpu-load wget R unrar
 brew install "${apps[@]}"
 
 print_info "installing apps through Homebrew Cask..."
-cask_apps=(mactex rstudio)
+cask_apps=(mactex rstudio macvim)
 brew cask install "${cask_apps[@]}"
