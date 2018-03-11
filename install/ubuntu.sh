963d7aa2fe9b0618ea84eaa5394a7905f8b330a8
stevenliuyi <stevenliuyi@gmail.com>
2018-03-11 01:12:26 -0500

    add Google Cloud SDK

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -5,7 +5,7 @@ print_info "updating apt..."
 sudo apt-get update
 
 # install packages
-packages="awscli binutils build-essential cmake curl gcc g++ make software-properties-common texlive texlive-generic-extra texlive-latex-extra tmux p7zip p7zip-rar python-pip zsh vim autojump r-base r-base-dev gdebi-core unrar shellcheck htop trash-cli silversearcher-ag nodejs mpich yarn octave oracle-java8-set-default android-sdk virtualbox gpupg"
+packages="awscli binutils build-essential cmake curl gcc g++ make software-properties-common texlive texlive-generic-extra texlive-latex-extra tmux p7zip p7zip-rar python-pip zsh vim autojump r-base r-base-dev gdebi-core unrar shellcheck htop trash-cli silversearcher-ag nodejs mpich yarn octave oracle-java8-set-default android-sdk virtualbox gpupg google-cloud-sdk"
 
 for pkg in $packages; do
     # check if the package is already installed
@@ -33,6 +33,16 @@ for pkg in $packages; do
         if [ "$pkg" == "oracle-java8-set-default" ]; then
             sudo add-apt-repository ppa:webupd8team/java
             sudo apt-get update
+        fi
+
+        # Google Cloud
+        if [ "$pkg" == "google-cloud-sdk"]; then
+            # Create an environment variable for the correct distribution
+            export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+            # Add the Cloud SDK distribution URI as a package source
+            echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+            # Import the Google Cloud Platform public key
+            curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
         fi
 
         if sudo apt-get -qq install "$pkg"; then
