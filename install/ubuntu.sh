130dc6052cd49b2c32e8065fb4518d0bfc08606b
stevenliuyi <stevenliuyi@gmail.com>
2018-03-16 19:46:21 -0400

    fix

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -36,13 +36,14 @@ for pkg in $packages; do
         fi
 
         # Google Cloud
-        if [ "$pkg" == "google-cloud-sdk"]; then
+        if [ "$pkg" == "google-cloud-sdk" ]; then
             # Create an environment variable for the correct distribution
             export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
             # Add the Cloud SDK distribution URI as a package source
             echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
             # Import the Google Cloud Platform public key
             curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+            sudo apt-get update
         fi
 
         if sudo apt-get -qq install "$pkg"; then
