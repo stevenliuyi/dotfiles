52efea8cf7c3925e5b07fffa95dae9639a414b5e
stevenliuyi <stevenliuyi@gmail.com>
2018-04-22 18:50:37 -0400

    add wikidata-cli

--- install/yarn.sh
+++ install/yarn.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # install yarn packages
-packages="create-react-app n heroku-cli create-react-native-app eslint"
+packages="create-react-app n heroku-cli create-react-native-app eslint wikidata-cli"
 for package in $packages; do
     yarn global add "$package"
 done
