f65e6a8216f4f55ad9df50c603e167d3c7e2911d
stevenliuyi <stevenliuyi@gmail.com>
2018-09-29 13:38:07 -0400

    add source-map-explorer

--- install/yarn.sh
+++ install/yarn.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # install yarn packages
-packages="create-react-app n heroku-cli create-react-native-app eslint wikidata-cli hexo-cli nodemon ngrok"
+packages="create-react-app n heroku-cli create-react-native-app eslint wikidata-cli hexo-cli nodemon ngrok source-map-explorer"
 for package in $packages; do
     yarn global add "$package"
 done
